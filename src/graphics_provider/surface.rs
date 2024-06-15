26fc219b6db23f1ad14d76b992ad92567d371a3c
DerGorn <dominik.schlothane@gmail.com>
2024-06-15 22:55:52 +0200

    split surface.rs into components

--- src/graphics_provider/surface.rs
+++ src/graphics_provider/surface.rs
@@ -1,8 +1,11 @@
-use crate::create_name_struct;
-
 use super::ShaderDescriptor;
 use std::fmt::Debug;
-use wgpu::util::DeviceExt;
+
+mod buffer_writer;
+pub use buffer_writer::BufferWriter;
+
+mod render_scene;
+pub use render_scene::{RenderScene, RenderSceneName};
 
 pub trait WindowSurface: Debug {
     fn surface<'a, 'b: 'a>(&'b self) -> &'a wgpu::Surface<'a>;
@@ -164,130 +167,5 @@ impl<'a> WindowSurface for Surface<'a> {
 
         queue.submit(std::iter::once(encoder.finish()));
         output.present();
-    }
-}
-
-create_name_struct!(RenderSceneName);
-
-pub struct RenderScene {
-    name: RenderSceneName,
-    render_pipeline: wgpu::RenderPipeline,
-    vertex_buffer: wgpu::Buffer,
-    index_buffer: wgpu::Buffer,
-    num_indices: u32,
-    num_vertices: u32,
-    index_format: wgpu::IndexFormat,
-}
-impl RenderScene {
-    pub fn new(
-        name: RenderSceneName,
-        render_pipeline: wgpu::RenderPipeline,
-        vertex_buffer: wgpu::Buffer,
-        index_buffer: wgpu::Buffer,
-        num_indices: u32,
-        num_vertices: u32,
-        index_format: wgpu::IndexFormat,
-    ) -> Self {
-        Self {
-            name,
-            render_pipeline,
-            vertex_buffer,
-            index_buffer,
-            num_indices,
-            num_vertices,
-            index_format,
-        }
-    }
-
-    pub fn update_pipeline(&mut self, render_pipeline: wgpu::RenderPipeline) {
-        self.render_pipeline = render_pipeline;
-    }
-
-    pub fn name(&self) -> &RenderSceneName {
-        &self.name
-    }
-
-    pub fn update(
-        &mut self,
-        device: &wgpu::Device,
-        queue: &wgpu::Queue,
-        vertices: &dyn BufferWriter,
-        indices: &dyn BufferWriter,
-    ) {
-        if let Some((index_buffer, num_indices)) = indices.write_buffer(
-            device,
-            queue,
-            &self.index_buffer,
-            self.num_indices,
-            wgpu::BufferUsages::INDEX,
-        ) {
-            self.index_buffer = index_buffer;
-            self.num_indices = num_indices;
-        };
-        if let Some((vertex_buffer, num_vertices)) = vertices.write_buffer(
-            device,
-            queue,
-            &self.vertex_buffer,
-            self.num_vertices,
-            wgpu::BufferUsages::VERTEX,
-        ) {
-            self.vertex_buffer = vertex_buffer;
-            self.num_vertices = num_vertices;
-        };
-    }
-
-    fn write_render_pass<'a>(
-        &'a self,
-        render_pass: &mut wgpu::RenderPass<'a>,
-        bind_groups: &[&'a wgpu::BindGroup],
-    ) {
-        render_pass.set_pipeline(&self.render_pipeline);
-        for (i, bind_group) in bind_groups.iter().enumerate() {
-            render_pass.set_bind_group(i as u32, bind_group, &[]);
-        }
-        render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-        render_pass.set_index_buffer(self.index_buffer.slice(..), self.index_format);
-        render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
-    }
-}
-pub trait BufferWriter {
-    fn buffer_data<'a>(&'a self) -> Option<&'a [u8]>;
-    fn buffer_len(&self) -> u32;
-
-    fn write_buffer(
-        &self,
-        device: &wgpu::Device,
-        queue: &wgpu::Queue,
-        buffer: &wgpu::Buffer,
-        buffer_len: u32,
-        usage: wgpu::BufferUsages,
-    ) -> Option<(wgpu::Buffer, u32)> {
-        if let Some(buffer_data) = self.buffer_data() {
-            let new_len = self.buffer_len();
-            if buffer_len < new_len {
-                let buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-                    label: Some(&format!("{:?} Buffer", usage)),
-                    contents: buffer_data,
-                    usage: usage | wgpu::BufferUsages::COPY_DST,
-                });
-                Some((buffer, new_len))
-            } else {
-                queue.write_buffer(buffer, 0, buffer_data);
-                None
-            }
-        } else {
-            None
-        }
-    }
-}
-impl<T> BufferWriter for Option<&[T]>
-where
-    T: bytemuck::Pod,
-{
-    fn buffer_len(&self) -> u32 {
-        self.unwrap_or_else(|| &[]).len() as u32
-    }
-    fn buffer_data<'a>(&'a self) -> Option<&'a [u8]> {
-        self.map(|s| bytemuck::cast_slice(s))
     }
 }
---
+++ src/graphics_provider/surface/render_scene.rs
@@ -0,0 +1,87 @@
+use crate::create_name_struct;
+
+use super::BufferWriter;
+
+create_name_struct!(RenderSceneName);
+
+pub struct RenderScene {
+    name: RenderSceneName,
+    render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
+    num_vertices: u32,
+    index_format: wgpu::IndexFormat,
+}
+impl RenderScene {
+    pub fn new(
+        name: RenderSceneName,
+        render_pipeline: wgpu::RenderPipeline,
+        vertex_buffer: wgpu::Buffer,
+        index_buffer: wgpu::Buffer,
+        num_indices: u32,
+        num_vertices: u32,
+        index_format: wgpu::IndexFormat,
+    ) -> Self {
+        Self {
+            name,
+            render_pipeline,
+            vertex_buffer,
+            index_buffer,
+            num_indices,
+            num_vertices,
+            index_format,
+        }
+    }
+
+    pub fn update_pipeline(&mut self, render_pipeline: wgpu::RenderPipeline) {
+        self.render_pipeline = render_pipeline;
+    }
+
+    pub fn name(&self) -> &RenderSceneName {
+        &self.name
+    }
+
+    pub fn update(
+        &mut self,
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        vertices: &dyn BufferWriter,
+        indices: &dyn BufferWriter,
+    ) {
+        if let Some((index_buffer, num_indices)) = indices.write_buffer(
+            device,
+            queue,
+            &self.index_buffer,
+            self.num_indices,
+            wgpu::BufferUsages::INDEX,
+        ) {
+            self.index_buffer = index_buffer;
+            self.num_indices = num_indices;
+        };
+        if let Some((vertex_buffer, num_vertices)) = vertices.write_buffer(
+            device,
+            queue,
+            &self.vertex_buffer,
+            self.num_vertices,
+            wgpu::BufferUsages::VERTEX,
+        ) {
+            self.vertex_buffer = vertex_buffer;
+            self.num_vertices = num_vertices;
+        };
+    }
+
+    pub fn write_render_pass<'a>(
+        &'a self,
+        render_pass: &mut wgpu::RenderPass<'a>,
+        bind_groups: &[&'a wgpu::BindGroup],
+    ) {
+        render_pass.set_pipeline(&self.render_pipeline);
+        for (i, bind_group) in bind_groups.iter().enumerate() {
+            render_pass.set_bind_group(i as u32, bind_group, &[]);
+        }
+        render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+        render_pass.set_index_buffer(self.index_buffer.slice(..), self.index_format);
+        render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
+    }
+}
